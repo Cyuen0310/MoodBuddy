6b91558c5b161fd1e182fa7cc39a529c176edfa8
Cyuen0310 <chanszeyuen0310@icloud.com>
2025-01-23 17:55:37 +0800

    test

---
+++ components/(journal)/journalCard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const journalCard = () => {
+  return (
+    <View>
+      <Text>journalCard</Text>
+    </View>
+  );
+};
+
+export default journalCard;
