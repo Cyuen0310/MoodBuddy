a94c39ad087dd245ec356a4276bbbb53751911a5
Cyuen0310 <chanszeyuen0310@icloud.com>
2025-02-26 12:27:46 +0800

    revert sighup

--- app/(auth)/SignUp.tsx
+++ app/(auth)/SignUp.tsx
@@ -40,6 +40,8 @@ import {
   TextLinkContent,
   PageTitle,
 } from "@/components/style/style";
+import { KeyboardAvoidingView } from "react-native";
+import { Keyboard } from "react-native";
 const { darkLight, brand } = Colors;
 
 interface FormValues {
@@ -109,213 +111,214 @@ const SignUpScreen = () => {
 
   return (
     <Styledcontainer>
+      <StatusBar style="dark" />
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
-        className="flex-1 p-5"
+        style={{ flex: 1 }}
       >
-      <StatusBar style="dark" />
-      <KeyboardAvoidingView 
-      behavior={Platform.OS === "ios" ? "padding" : "height"} 
-      style={{ flex: 1 }}>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <ScrollView 
-            contentContainerStyle={{ flexGrow: 1 }} 
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
             keyboardShouldPersistTaps="handled"
-          >        
-          <InnerContainer>
-          <PageTitle>Mood Buddy</PageTitle>
-          <SubTitle>Sign Up</SubTitle>
-          <Formik
-            initialValues={{
-              username: "",
-              fullname: "",
-              email: "",
-              dob: dob.toLocaleDateString(),
-              password: "",
-              gender: "",
-              confirmpassword: "",
-            }}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
           >
-            {({
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              errors,
-              values,
-              touched,
-            }) => (
-              <StyledFromArea>
-                <UserTextInput
-                  label="User Name *"
-                  icon={require("@/assets/images/user.png")}
-                  placeholder="User Name"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("username")}
-                  onBlur={handleBlur("username")}
-                  value={values.username}
-                />
-                {errors.username && touched.username && (
-                  <Text style={{ color: "red" }}>{errors.username}</Text>
-                )}
+            <InnerContainer>
+              <PageTitle>Mood Buddy</PageTitle>
+              <SubTitle>Sign Up</SubTitle>
+              <Formik
+                initialValues={{
+                  username: "",
+                  fullname: "",
+                  email: "",
+                  dob: dob.toLocaleDateString(),
+                  password: "",
+                  gender: "",
+                  confirmpassword: "",
+                }}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+              >
+                {({
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  errors,
+                  values,
+                  touched,
+                }) => (
+                  <StyledFromArea>
+                    <UserTextInput
+                      label="User Name *"
+                      icon={require("@/assets/images/user.png")}
+                      placeholder="User Name"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("username")}
+                      onBlur={handleBlur("username")}
+                      value={values.username}
+                    />
+                    {errors.username && touched.username && (
+                      <Text style={{ color: "red" }}>{errors.username}</Text>
+                    )}
 
-                <UserTextInput
-                  label="Full Name"
-                  icon={require("@/assets/images/user.png")}
-                  placeholder="Full Name"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("fullname")}
-                  onBlur={handleBlur("fullname")}
-                  value={values.fullname}
-                />
+                    <UserTextInput
+                      label="Full Name"
+                      icon={require("@/assets/images/user.png")}
+                      placeholder="Full Name"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("fullname")}
+                      onBlur={handleBlur("fullname")}
+                      value={values.fullname}
+                    />
 
-                <UserTextInput
-                  label="Email Address*"
-                  icon={require("@/assets/images/email.png")}
-                  placeholder="user@example.com"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("email")}
-                  onBlur={handleBlur("email")}
-                  value={values.email}
-                  keyboardType="email-address"
-                />
-                {errors.email && touched.email && (
-                  <Text style={{ color: "red" }}>{errors.email}</Text>
-                )}
+                    <UserTextInput
+                      label="Email Address*"
+                      icon={require("@/assets/images/email.png")}
+                      placeholder="user@example.com"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("email")}
+                      onBlur={handleBlur("email")}
+                      value={values.email}
+                      keyboardType="email-address"
+                    />
+                    {errors.email && touched.email && (
+                      <Text style={{ color: "red" }}>{errors.email}</Text>
+                    )}
 
-                <UserTextInput
-                  label="Date of Birth"
-                  icon={require("@/assets/images/cake.png")}
-                  placeholder="DD - MM - YYYY"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("DOB")}
-                  onBlur={handleBlur("DOB")}
-                  value={dob.toLocaleDateString()}
-                  onFocus={() => {
-                    Keyboard.dismiss();
-                    setShowDatePicker(true);
-                  }}
-                  />
-                {showDatePicker && (
-                  <DateTimePicker
-                    value={dob}
-                    mode="date"
-                    display="default"
-                    onChange={onChangeDob}
-                    maximumDate={new Date()}
-                  />
-                )}
-
-                <View>
-                  <StyledInputLabel>Gender*</StyledInputLabel>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      justifyContent: "space-around",
-                    }}
-                  >
-                    <TouchableOpacity
-                      onPress={() => {
-                        setGender("male");
-                        handleChange("gender")("male");
+                    <UserTextInput
+                      label="Date of Birth"
+                      icon={require("@/assets/images/cake.png")}
+                      placeholder="DD - MM - YYYY"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("DOB")}
+                      onBlur={handleBlur("DOB")}
+                      value={dob.toLocaleDateString()}
+                      onFocus={() => {
+                        Keyboard.dismiss();
+                        setShowDatePicker(true);
                       }}
-                    >
-                      <Ionicons
-                        name="male"
-                        size={30}
-                        color={gender === "male" ? "blue" : "gray"}
+                    />
+                    {showDatePicker && (
+                      <DateTimePicker
+                        value={dob}
+                        mode="date"
+                        display="default"
+                        onChange={onChangeDob}
+                        maximumDate={new Date()}
                       />
-                      <Text>Male</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      onPress={() => {
-                        setGender("female");
-                        handleChange("gender")("female");
-                      }}
-                    >
-                      <Ionicons
-                        name="female"
-                        size={30}
-                        color={gender === "female" ? "blue" : "gray"}
-                      />
-                      <Text>Female</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      onPress={() => {
-                        setGender("other");
-                        handleChange("gender")("other");
-                      }}
-                      style={{ alignItems: "center" }}
-                    >
-                      <Image
-                        source={require("@/assets/images/genderless.png")}
+                    )}
+
+                    <View>
+                      <StyledInputLabel>Gender*</StyledInputLabel>
+                      <View
                         style={{
-                          width: 30,
-                          height: 30,
-                          tintColor: gender === "other" ? "blue" : "gray",
+                          flexDirection: "row",
+                          justifyContent: "space-around",
                         }}
-                      />
-                      <Text>Other</Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-                {errors.gender && touched.gender && (
-                  <Text style={{ color: "red" }}>{errors.gender}</Text>
-                )}
+                      >
+                        <TouchableOpacity
+                          onPress={() => {
+                            setGender("male");
+                            handleChange("gender")("male");
+                          }}
+                        >
+                          <Ionicons
+                            name="male"
+                            size={30}
+                            color={gender === "male" ? "blue" : "gray"}
+                          />
+                          <Text>Male</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                          onPress={() => {
+                            setGender("female");
+                            handleChange("gender")("female");
+                          }}
+                        >
+                          <Ionicons
+                            name="female"
+                            size={30}
+                            color={gender === "female" ? "blue" : "gray"}
+                          />
+                          <Text>Female</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                          onPress={() => {
+                            setGender("other");
+                            handleChange("gender")("other");
+                          }}
+                          style={{ alignItems: "center" }}
+                        >
+                          <Image
+                            source={require("@/assets/images/genderless.png")}
+                            style={{
+                              width: 30,
+                              height: 30,
+                              tintColor: gender === "other" ? "blue" : "gray",
+                            }}
+                          />
+                          <Text>Other</Text>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                    {errors.gender && touched.gender && (
+                      <Text style={{ color: "red" }}>{errors.gender}</Text>
+                    )}
 
-                <UserTextInput
-                  label="Password*"
-                  icon={require("@/assets/images/padlock.png")}
-                  placeholder="* * * * * *"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("password")}
-                  onBlur={handleBlur("password")}
-                  value={values.password}
-                  secureTextEntry={hidePassword}
-                  isPassword={true}
-                  hidePassword={hidePassword}
-                  setPassword={setPassword}
-                />
-                {errors.password && touched.password && (
-                  <Text style={{ color: "red" }}>{errors.password}</Text>
-                )}
+                    <UserTextInput
+                      label="Password*"
+                      icon={require("@/assets/images/padlock.png")}
+                      placeholder="* * * * * *"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("password")}
+                      onBlur={handleBlur("password")}
+                      value={values.password}
+                      secureTextEntry={hidePassword}
+                      isPassword={true}
+                      hidePassword={hidePassword}
+                      setPassword={setPassword}
+                    />
+                    {errors.password && touched.password && (
+                      <Text style={{ color: "red" }}>{errors.password}</Text>
+                    )}
 
-                <UserTextInput
-                  label="Confirm Password*"
-                  icon={require("@/assets/images/padlock.png")}
-                  placeholder="* * * * * *"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("confirmpassword")}
-                  onBlur={handleBlur("confirmpassword")}
-                  value={values.confirmpassword}
-                  secureTextEntry={hideCPassword}
-                  isPassword={true}
-                  hidePassword={hideCPassword}
-                  setPassword={setCPassword}
-                />
-                {errors.confirmpassword && touched.confirmpassword && (
-                  <Text style={{ color: "red" }}>{errors.confirmpassword}</Text>
-                )}
-                {errorMessage ? (
-                  <MsgBox style={{ color: "red" }}>{errorMessage}</MsgBox>
-                ) : null}
+                    <UserTextInput
+                      label="Confirm Password*"
+                      icon={require("@/assets/images/padlock.png")}
+                      placeholder="* * * * * *"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("confirmpassword")}
+                      onBlur={handleBlur("confirmpassword")}
+                      value={values.confirmpassword}
+                      secureTextEntry={hideCPassword}
+                      isPassword={true}
+                      hidePassword={hideCPassword}
+                      setPassword={setCPassword}
+                    />
+                    {errors.confirmpassword && touched.confirmpassword && (
+                      <Text style={{ color: "red" }}>
+                        {errors.confirmpassword}
+                      </Text>
+                    )}
+                    {errorMessage ? (
+                      <MsgBox style={{ color: "red" }}>{errorMessage}</MsgBox>
+                    ) : null}
 
-                <StyledButton onPress={() => handleSubmit()}>
-                  <ButtonText>Next</ButtonText>
-                </StyledButton>
-                <Line />
-                <ExtraView>
-                  <ExtraText> Have an account already? </ExtraText>
-                  <TextLink onPress={() => router.replace("/Login")}>
-                    <TextLinkContent>Login</TextLinkContent>
-                  </TextLink>
-                </ExtraView>
-              </StyledFromArea>
-            )}
-          </Formik>
-        </InnerContainer>
-      </ScrollView>
+                    <StyledButton onPress={() => handleSubmit()}>
+                      <ButtonText>Next</ButtonText>
+                    </StyledButton>
+                    <Line />
+                    <ExtraView>
+                      <ExtraText> Have an account already? </ExtraText>
+                      <TextLink onPress={() => router.replace("/Login")}>
+                        <TextLinkContent>Login</TextLinkContent>
+                      </TextLink>
+                    </ExtraView>
+                  </StyledFromArea>
+                )}
+              </Formik>
+            </InnerContainer>
+          </ScrollView>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </Styledcontainer>
   );
 };
@@ -388,4 +391,3 @@ const UserTextInput = ({
 };
 
 export default SignUpScreen;
-
